d52d3935f6e33a97f3aaa95236bcdd7af1eb6103
pledrogo <patrick.ledrogo@contentsquare.com>
2019-06-03 20:01:58 +0200

    upg slack cleint

--- services/slack_queries.py
+++ services/slack_queries.py
@@ -1,18 +1,19 @@
 import os
-from slackclient import SlackClient
+import slack
 
 def send(channel, text, username, icon_url):
     webhook_url = 'https://slack.com/api/chat.postMessage'
     token = os.environ.get('SLACK_MSG_TOKEN','N/A')
-    sc = SlackClient(token)
+    client = slack.WebClient(token=token)
 
-    response = sc.api_call(
-        "chat.postMessage",
+
+    response = client.chat_postMessage(
         channel=channel,
         text=text,
         as_user='false',
         username=username,
-        icon_url=icon_url
+        icon_url=icon_url,
+
     )
 
     return response['ok']
